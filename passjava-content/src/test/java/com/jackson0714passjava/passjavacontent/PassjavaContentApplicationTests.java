fb79c8fae6dfa571ed9a05d221febc691ba442ef
xueliangdbc <yuxueliang@corp.netease.com>
2021-02-01 11:16:05 +0800

    first commit

---
+++ passjava-content/src/test/java/com/jackson0714passjava/passjavacontent/PassjavaContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackson0714passjava.passjavacontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaContentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
